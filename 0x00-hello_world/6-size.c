73fe23a27e3783091861efa8e9be76f0b991115f
Bakkali1 <montassir_b@hotmail.com>
2023-03-16 17:12:51 -0700

    size

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	prinf("Size of a char: %lu  byte(s)", sizeof(char)\n);
+	prinf("Size of an int: %lu  byte(s)", sizeof(int)\n);
+	prinf("Size of a long int: %lu  byte(s)", sizeof(long int)\n);
+	prinf("Size of a long long int: %lu  byte(s)", sizeof(long long int)\n);
+	prinf("Size of a float: %lu  byte(s)", sizeof(float)\n);
+	return (0);
+}
